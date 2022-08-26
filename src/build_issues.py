541576b94564d33357ce801a850f54b01c773946
Jay Miller <kjaymiller@gmail.com>
2022-08-26 13:37:22 -0700

    move files all under app

--- src/build_issues.py
+++
@@ -1,6 +0,0 @@
-"""Takes Issues Filed in the last week and puts them in a file for the upcoming stream"""
-import pathlib
-import github
-from datetime import datetime
-
-# Get the date of the next friday
