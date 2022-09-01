8b0a1c1a36962cbf4000c847a1fce04467b8d657
Jon Banafato <jon@jonafato.com>
2022-08-31 23:21:17 -0400

    Run pre-commit on all files

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,27 @@
-from render_engine.site import Site
 from render_engine.blog import Blog
 from render_engine.page import Page
+from render_engine.site import Site
 
 
 class Site(Site):
-    output_path = 'output'
+    output_path = "output"
     site_vars: dict = {
         "SITE_TITLE": "Python Community News",
-        "SITE_URL":  "https://pythoncommunitynews.com"
+        "SITE_URL": "https://pythoncommunitynews.com",
     }
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     site = Site(static="static")
-    
+
     @site.render_page
     class index(Page):
-        template = 'index.html'
+        template = "index.html"
 
     @site.render_collection
     class archive(Blog):
         has_archive = True
-        output_path = './'
-        content_path = './app/content'
-        template = 'new_post.html'
-        archive_template: str = 'archive.html'
+        output_path = "./"
+        content_path = "./app/content"
+        template = "new_post.html"
+        archive_template: str = "archive.html"
