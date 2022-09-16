242cd013bbd64868d0bba8c1b8edaf3c6f5e5f90
Jay Miller <kjaymiller@gmail.com>
2022-09-16 13:30:31 -0400

    adds 2022-09-16

--- site/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["site/templates/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')]
-}
